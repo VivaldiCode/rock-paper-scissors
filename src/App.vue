5ade17e0826c3efb81e288924ef0d0227deed962
VivaldiCode <guilhermecamachop@gmail.com>
2024-01-07 13:05:52 +0000

    basic game

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <script lang="ts">
+import HeaderComponent from '@/components/HeaderComponent.vue'
+
 interface GameData {
   choices: string[];
   userChoice: string;
   computerChoice: string;
   result: string;
+  resultClass: string;
   userScore: number;
   computerScore: number;
+  isActive: boolean;
+
 }
 
 export default {
+  components: { HeaderComponent },
   data(): GameData {
     return {
       choices: ['rock', 'paper', 'scissors', 'lizard', 'spock'],
       userChoice: '',
       computerChoice: '',
       result: '',
+      resultClass: '',
       userScore: 0,
-      computerScore: 0
+      computerScore: 0,
+      isActive: false
     }
   },
+
   methods: {
     playGame(): void {
-      this.computerChoice = this.choices[Math.floor(Math.random() * this.choices.length)];
+      this.computerChoice = this.choices[Math.floor(Math.random() * this.choices.length)]
       // Logic for Rock, Paper, Scissors, Lizard, Spock
       if (this.userChoice === this.computerChoice) {
         this.result = 'It\'s a tie!'
+        this.resultClass = 'tie'
       } else if (
         (this.userChoice === 'rock' && (this.computerChoice === 'scissors' || this.computerChoice === 'lizard')) ||
         (this.userChoice === 'paper' && (this.computerChoice === 'rock' || this.computerChoice === 'spock')) ||
@@ -33,22 +43,35 @@ export default {
         (this.userChoice === 'spock' && (this.computerChoice === 'rock' || this.computerChoice === 'scissors'))
       ) {
         this.result = 'You win!'
+        this.resultClass = 'win'
+
         this.userScore++
       } else {
         this.result = 'You lose!'
-        this.computerScore++
+        this.resultClass = 'lose'
+
+        if (this.userScore > 0) this.userScore--
       }
+    },
+    makeUserChoice(choice: string): void {
+      this.userChoice = choice
+      this.isActive = true
+      document.body.classList.add('modal-active')
+      this.playGame()
     },
     resetGame(): void {
       this.userChoice = ''
       this.computerChoice = ''
       this.result = ''
-      this.userScore = 0
-      this.computerScore = 0
+      this.resultClass = ''
+      this.isActive = false
+      document.body.classList.remove('modal-active')
+
+
     },
     capitalizeFirstLetter(word: string): string {
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    },
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }
   },
   mounted(): void {
     // Fetch saved scores from local storage for the bonus feature
@@ -58,30 +81,45 @@ export default {
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <div>
-        <h1>Rock, Paper, Scissors</h1>
-        <div>
-          <button v-for="choice in choices" :key="choice" @click="userChoice = choice">{{ capitalizeFirstLetter(choice) }}</button>
-
-        </div>
-        <div>
-          <p v-if="userChoice">Your Choice: {{ capitalizeFirstLetter(userChoice) }}</p>
-          <p v-if="computerChoice">Computer's Choice: {{ capitalizeFirstLetter(computerChoice) }}</p>
-          <p v-if="result">{{ result }}</p>
-        </div>
-        <div>
-          <p>User Score: {{ userScore }}</p>
-          <p>Computer Score: {{ computerScore }}</p>
-        </div>
-        <div>
-          <button @click="playGame">Play</button>
-          <button @click="resetGame">Reset</button>
-        </div>
+  <div class="wrapper">
+    <div>
+      <HeaderComponent :userScore="userScore" :computerScore="computerScore" />
+      <div class="choice choice-l1">
+        <button class="scissors" @click="makeUserChoice('scissors')"></button>
+      </div>
+      <div class="choice choice-l2">
+        <button class="spock" @click="makeUserChoice('spock')"></button>
+        <button class="paper" @click="makeUserChoice('paper')"></button>
+      </div>
+      <div class="choice choice-l3">
+        <button class="lizard" @click="makeUserChoice('lizard')"></button>
+        <button class="rock" @click="makeUserChoice('rock')"></button>
       </div>
     </div>
-  </header>
+  </div>
+  <div id="modal-container" :class="{ active: isActive, four: isActive }">
+    <div class="modal-background">
+      <div class="modal">
+        <div class="result">
+          <div class="result-button">
+            <h1 v-if="userChoice">You Pick:</h1>
+            <div class="choice choice-result">
+              <button :class="userChoice + ' '  + ((resultClass === 'win' || resultClass === 'tie') ? 'wave' : '')"></button>
+            </div>
+          </div>
+          <div class="result-status">
+            <h1 v-if="result">{{ result }}</h1>
+            <button @click="resetGame">Play Again</button>
+          </div>
+          <div class="result-button">
+            <h1 v-if="computerChoice">House Pick:</h1>
+            <div class="choice choice-result">
+              <button :class="computerChoice + ' ' + ((resultClass === 'lose' || resultClass === 'tie') ? 'wave' : '')"></button>
+            </div>
+          </div>
+        </div>
 
-
+      </div>
+    </div>
+  </div>
 </template>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  userScore: number,
+  computerScore: number,
+}>()
+</script>
+
+<template>
+  <div class="header">
+    <div class="title">
+      <h1>Rock<br/> Paper<br/> Scissors</h1>
+    </div>
+    <div class="score">
+      <span>Score</span>
+      <h1>{{ props.userScore }}</h1>
+<!--      <span>Computer Score: {{ props.computerScore }}</span>-->
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
